955e0bb2e21473dde56f0caf973cb2272b7e1456
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-04-10 20:25:20 +0330

    Add programmer and developer profile

--- CountingAdjacentFileExtensions.py
+++ CountingAdjacentFileExtensions.py
@@ -20,3 +20,7 @@ for file in file_list:
 print("the number of files with extensions: ")
 for extension, count in file_count_dict.items():
     print(extension + ':' + str(count))
+
+# Name of the programmer: Maryam Jamali
+# Email address: m.jamali16@yahoo.com
+# GitHub address: https://github.com/MaryaJamali
